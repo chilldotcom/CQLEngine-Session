734c7215b51cf363d6b363bbec0d60669c4ebc15
Michael Cyrulnik <lifto@mac.com>
2013-10-24 08:36:02 -0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cqlengine',
+    version='0.1',
+    description="Session with identity map for cqlengine",
+    long_description="Session with identity map for cqlengine",
+    classifiers = [
+        "Environment :: Web Environment",
+        "Environment :: Plugins",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    keywords='cassandra,cql,orm',
+    install_requires = ['cqlengine'],
+    author='Michael Cyrulnik',
+    author_email='michael@chill.com',
+    url='https://github.com/chilldotcom/CQLEngine-Session',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+)
