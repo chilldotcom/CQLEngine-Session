b9cb6497f7a65e5930f4d33f76d026623fd8c2c0
Michael Cyrulnik <lifto@mac.com>
2013-10-31 23:16:58 -0400

    new global session (only), new SessionModel class for models to inherit from

--- cqlengine_session.py
+++ cqlengine_session.py
@@ -1,203 +1,134 @@
 
-class _SessionedClass(object):
-    """Wraps a cqlengine class for use in a Session."""
+import threading
+from cqlengine.exceptions import ValidationError
+from cqlengine.models import BaseModel, ModelMetaClass
+from cqlengine.query import BatchQuery
 
-    def __init__(self, session, wrapped_class, **kwargs):
-        # Because _SessionClass overrides __setattr__, if you want to set
-        # non-cqlengine-column attributes, use object.__setattr__
-        object.__setattr__(self, 'session', session)
-        object.__setattr__(self, 'wrapped_class', wrapped_class)
-        for name, column in wrapped_class._columns.iteritems():
+SESSION = threading.local()
+
+def get_session(clear=False, create_if_missing=True):
+    """Gets the global session, making a new one if one does not already exist.
+
+    clear -- If True, make a new global session and return it
+    create_if_missing -- If False, will return None if session does not exist.
+
+    """
+    session = None
+    if not clear:
+        try:
+            session = SESSION.value
+        except AttributeError:
+            pass
+    if not session and create_if_missing:
+        session = Session()
+        SESSION.value = session
+    return session
+
+def save(*args, **kwargs):
+    "Write all pending changes from session to Cassandra."
+    get_session().save(*args, **kwargs)
+
+class Session(object):
+    """Identity map objects and support for implicit batch save."""
+    def __init__(self):
+        self.instances_by_class = {}
+        self.creates = set()
+        self.updates = set()
+        self.deletes = set()
+
+    def save(self):
+        creates = self.creates - self.deletes
+        updates = self.updates - self.creates - self.deletes
+        with BatchQuery() as batch:
+            for create in creates:
+                values = {n: getattr(create, n) for n in create._columns.keys()}
+                create.__class__.batch(batch).create(**values)
+            for update in updates:
+                raise NotImplementedError
+            for delete in self.deletes:
+                raise NotImplementedError
+
+class SessionModelMetaClass(ModelMetaClass):
+    def __call__(cls, **values):
+        # Look up this object in the identity map, if it exists return it, if
+        # not, instantiate it, add it to the identity map, and return it.
+        # xxx - session only works with single-primary key objects at present.
+        print 'session model meta class __call__'
+        print '{} call {}'.format(cls, values)
+        key_name = cls._primary_keys.keys()[0]
+        try:
+            key = values[key_name]
+        except KeyError:
+            raise ValueError(u'Tried to instantiate {} without key {}'.format(
+                    cls.__name__, key_name))
+        session = get_session()
+        try:
+            instances_by_key = session.instances_by_class[cls.__name__]
+        except KeyError:
+            pass
+        else:
             try:
-                kwarg_val = kwargs[name]
-                object.__setattr__(self, name, kwarg_val)
+                instance = instances_by_key[key]
+            except KeyError:
+                pass
+            else:
+                # For each value in values, set it on the object if it is
+                # not already there.
+                for column_name, value in values.items():
+                    if column_name == key_name:
+                        continue
+                    # Get the value manager for the attribute.
+                    manager = instance._values[column_name]
+                    # If the value is unset, set the value.
+                    # xxx how do we know if the value has been set?
+                    setattr(instance, column_name, value)
+                return instance
+
+        # If we get here, that means we did not find an instance.
+        instance = super(SessionModelMetaClass, cls).__call__(**values)
+        try:
+            instances_by_key = session.instances_by_class[cls.__name__]
+        except KeyError:
+            instances_by_key = {}
+            session.instances_by_class[cls.__name__] = instances_by_key
+        instances_by_key[key] = instance
+
+        return instance
+
+
+class SessionModel(BaseModel):
+    __abstract__ = True
+    __metaclass__ = SessionModelMetaClass
+
+    # Override 'create' so that it does not call the query, but does let the
+    # session know to insert the object.
+    @classmethod
+    def session_create(cls, **kwargs):
+        print 'SessionModel.create'
+        extra_columns = set(kwargs.keys()) - set(cls._columns.keys())
+        if extra_columns:
+            raise ValidationError("Incorrect columns passed: {}".format(extra_columns))
+
+        # Here we do sessioned create.  the cqlengine
+        # will just do a save immediately, we want to hold off the save
+        # until session.save() and we want it to not save if the session
+        # never saves (is replaced by a new session aka cleared)
+        create_values = {}
+        for name, column in cls._columns.items():
+            try:
+                value = kwargs[name]
             except KeyError:
                 if column.default:
                     if callable(column.default):
-                        object.__setattr__(self, name, column.default())
+                        value = column.default()
                     else:
-                        object.__setattr__(self, name, column.default)
-                elif name in wrapped_class._primary_keys:
-                    raise ValueError(u"Can't create {} without providing primary key {}".format(wrapped_class, name))
+                        value = column.default
+                elif name in cls._primary_keys:
+                    raise ValueError(u"Can't create {} without providing primary key {}".format(cls.__name__, name))
                 else:
-                    object.__setattr__(self, name, None)
-
-    def __setattr__(self, name, value):
-        # Let's not create a dirty dict until a write is made.  We made load
-        # massive numbers of these objects and it would be good if the
-        # overhead of doing that was minimal.
-        try:
-            self.dirties[name] = value
-        except AttributeError:
-            object.__setattr__(self, 'dirties', {name: value})
-        self.session.dirties.add(self)
-        object.__setattr__(self, name, value)
-
-class Session(object):
-
-    def __init__(self):
-        self.creates = set()
-        self.dirties = set()
-
-    def commit(self):
-        """Write all pending changes to cqlengine."""
-        for obj in self.creates:
-            wrapped_obj = obj.wrapped_class.create(**{k: getattr(obj, k) for k in obj.wrapped_class._columns.keys()})
-            wrapped_obj.save()
-        for obj in (o for o in self.dirties if o not in self.creates):
-            # TODO: want to do blind writes, but I don't have that know-how yet.
-            key_name = obj.wrapped_class._primary_keys.keys()[0]
-            key = getattr(obj, key_name)
-            wrapped_obj = obj.wrapped_class.objects(**{key_name: key}).get()
-            for name, value in obj.dirties.iteritems():
-                setattr(wrapped_obj, name, value)
-            wrapped_obj.save()
-
-    def create(self, model_class, **kwargs):
-        """Create instance of model_class and return an identity mapped handle.
-
-        model_class -- Class that will be created when session is flushed.
-
-        returns identity map handle to instance.
-
-        """
-        item = _SessionedClass(self, model_class, **kwargs)
-        self.creates.add(item)
-        return item
-
-    def load(self, *args, **kwargs):
-        """Fetch row from Cassandra and returned wrapped object."""
-        inventory = kwargs.get('inventory')
-        keys = kwargs.get('keys')
-        if len(args) > 2:
-            raise TypeError("load() takes at most 2 arguments ({} given)".format(len(args)))
-        if len(args) == 2:
-            model_class = args[0]
-            keys = [args[1]]
-        elif len(args) == 1:
-            #if isinstance(args[0], _SessionedClass):
-                #xxxx
-            #else:
-                # _handle = args[0]
-                # model_class = _handle.model_class
-                # keys = [_handle.key]
-            raise NotImplementedError('need to be able to load with session.handle')
-        # Get the object from cqlengine.
-        # (note: I don't like that we are copying from cqlengine instead of
-        # just fetching on our own.  I don't like that cqlengine gets all the
-        # data yet we may want to only get some.
-        # TODO: Do we check if it is already loaded, and thus spare a second
-        # load?  Or do we assume that the second load is naively requested
-        # and only fetch it if the data is not already in the identity map?
-        # also, if data has already been set, and then we load, are we
-        # sure to preserve the pre-existing data?
-        if len(keys) > 1:
-            raise NotImplementedError('can only handle single key primary keys')
-        if len(model_class._primary_keys) > 1:
-            raise NotImplementedError('can only handle single primary key objects')
-        primary_key_name, _ = list(model_class._primary_keys.iteritems())[0]
-        primary_key = keys[0]
-        obj = model_class.objects(**{primary_key_name: primary_key}).get()
-        wrapped_obj = _SessionedClass(self, model_class, **{k: getattr(obj, k) for k in model_class._columns.keys()})
-        return wrapped_obj
-
-
-class sessionmaker(object):
-    """A configurable :class:`.Session` factory.
-
-    The :class:`.sessionmaker` factory generates new
-    :class:`.Session` objects when called, creating them given
-    the configurational arguments established here.
-
-    e.g.::
-
-        # global scope
-        Session = sessionmaker()
-
-        # later, in a local scope, create and use a session:
-        sess = Session()
-
-    XXX the spirit is correct here but the details need to be changed. - MEC
-
-    Any keyword arguments sent to the constructor itself will override the
-    "configured" keywords::
-
-        Session = sessionmaker()
-
-        # bind an individual session to a connection
-        sess = Session(bind=connection)
-
-    The class also includes a method :meth:`.configure`, which can
-    be used to specify additional keyword arguments to the factory, which
-    will take effect for subsequent :class:`.Session` objects generated.
-    This is usually used to associate one or more :class:`.Engine` objects
-    with an existing :class:`.sessionmaker` factory before it is first
-    used::
-
-        # application starts
-        Session = sessionmaker()
-
-        # ... later
-        engine = create_engine('sqlite:///foo.db')
-        Session.configure(bind=engine)
-
-        sess = Session()
-
-    .. seealso:
-
-        :ref:`session_getting` - introductory text on creating
-        sessions using :class:`.sessionmaker`.
-
-    """
-
-    def __init__(self, class_=Session, **kw):
-        """Construct a new :class:`.sessionmaker`.
-
-        All arguments here except for ``class_`` correspond to arguments
-        accepted by :class:`.Session` directly.  See the
-        :meth:`.Session.__init__` docstring for more details on parameters.
-
-        :param class_: class to use in order to create new :class:`.Session`
-         objects.  Defaults to :class:`.Session`.
-        :param \**kw: all other keyword arguments are passed to the constructor
-         of newly created :class:`.Session` objects.
-
-        """
-        self.kw = kw
-        # make our own subclass of the given class, so that
-        # events can be associated with it specifically.
-        self.class_ = type(class_.__name__, (class_,), {})
-
-    def __call__(self, **local_kw):
-        """Produce a new :class:`.Session` object using the configuration
-        established in this :class:`.sessionmaker`.
-
-        In Python, the ``__call__`` method is invoked on an object when
-        it is "called" in the same way as a function::
-
-            Session = sessionmaker()
-            session = Session()  # invokes sessionmaker.__call__()
-
-        """
-        for k, v in self.kw.items():
-            local_kw.setdefault(k, v)
-        return self.class_(**local_kw)
-
-    def configure(self, **new_kw):
-        """(Re)configure the arguments for this sessionmaker.
-
-        e.g.::
-
-            Session = sessionmaker()
-
-            Session.configure(bind=create_engine('sqlite://'))
-        """
-        self.kw.update(new_kw)
-
-    def __repr__(self):
-        return "%s(class_=%r%s)" % (
-                    self.__class__.__name__,
-                    self.class_.__name__,
-                    ", ".join("%s=%r" % (k, v) for k, v in self.kw.items())
-                )
+                    value = None
+            create_values[name] = value
+        instance = cls(**create_values)
+        get_session().creates.add(instance)
+        return instance
 
