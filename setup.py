65bc64e5d6b5002b1f4dbc25b09d83849bcc59fb
Michael Cyrulnik <lifto@mac.com>
2013-12-21 21:35:56 -0500

    bump cqlengine requirement to 0.10

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     keywords='cassandra,cql,orm',
-    install_requires = ['cqlengine>=0.9'],
+    install_requires = ['cqlengine>=0.10'],
     py_modules=[
         'cqlengine_session',
     ],
